dc58267a59d3d35ceffab1073915340cf01d3333
Waiwhakaira Falwasser <waifalcode@gmail.com>
2025-05-02 22:16:03 +1200

    Add PHPDoc comment for validateUser()

--- php/includes/functions.inc.php
+++ php/includes/functions.inc.php
@@ -1,5 +1,11 @@
 <?php
 
+/**
+ * Sanitizes and cleans user input, allowing only letters.
+ *
+ * @param ?string $userInput The input string, which may be null.
+ * @return ?string The sanitized string or null if input is empty.
+ */
 
 function validateUser(?string $userInput): ?string {
 	if ($userInput === null) {
