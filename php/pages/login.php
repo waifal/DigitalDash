c14c695b6e5b02e1b26098d41258d74826fb279e
Waiwhakaira Falwasser <waifalcode@gmail.com>
2025-05-02 19:17:19 +1200

    Add `login.php` and `signup.php` files

---
+++ php/pages/login.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+
+<html lang="en">
+
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>DigitalDash</title>
+
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css">
+	<link rel="stylesheet" href="../../css/styles.css">
+</head>
+
+<body>
+	<!-- Navigation -->
+	<?php require_once(__DIR__ . '/../components/nav.inc.php'); ?>
+
+	<main>
+		<section class='auth__container'>
+			<!-- Form -->
+			<div></div>
+			<!-- Background -->
+			<div class='auth-col-bg'></div>
+		</section>
+	</main>
+
+	<!-- Footer -->
+	<?php require_once(__DIR__ . '/../components/footer.inc.php'); ?>
+</body>
+
+</html>
---
+++ php/pages/signup.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+
+<html lang="en">
+
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>DigitalDash</title>
+
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css">
+	<link rel="stylesheet" href="../../css/styles.css">
+</head>
+
+<body>
+	<!-- Navigation -->
+	<?php require_once(__DIR__ . '/../components/nav.inc.php'); ?>
+
+	<main>
+		<section class='auth__container'>
+			<!-- Form -->
+			<div>
+				<h2>Explore the Wild, Anywhere.</h2>
+				<p>Join today to unlock breathtaking views and immersive trails, experiencing the beauty of nature like never before - right from your screen.</p>
+				<form id="signupfrm" action="../includes/signout.inc.php" method="POST">
+					<!-- Names -->
+					<div>
+						<label>First Name
+							<input type="text" name="fname" id="fname" autocomplete="given-name" placeholder="Enter your First Name" required>
+						</label>
+						<label>Last Name
+							<input type="text" name="lname" id="lname" autocomplete="family-name" placeholder="Enter your Last Name" required>
+						</label>
+					</div>
+					<!-- Email Address -->
+					<div>
+						<label>Email Address
+							<input type="email" name="email" id="email" autocomplete="email" placeholder="Enter your Email Address" required>
+						</label>
+					</div>
+					<!-- Password -->
+					<div>
+						<label>Your Password
+							<input type="password" name="password" id="password" autocomplete="new-password" placeholder="Enter your Password" required>
+						</label>
+						<label>Confirm Password
+							<input type="password" name="pwd_confirm" id="pwd_confirm" autocomplete="new-password" placeholder="Confirm your Password" required>
+						</label>
+					</div>
+					<!-- Agreements -->
+					<div>
+						<label>
+							<input type="checkbox" name="terms_and_conditions" id="terms_and_conditions" required>
+							<a href="#">Terms & Conditions</a>
+						</label>
+						<label>
+							<input type="checkbox" name="privacy_policy" id="privacy_policy" required>
+							<a href="#">Privacy Policy</a>
+						</label>
+					</div>
+					<!-- Buttons -->
+					<div class='button__container'>
+						<a href="../includes/signup.inc.php">Sign up</a>
+						<a href="../pages/login.php">Login</a>
+					</div>
+				</form>
+			</div>
+			<!-- Background -->
+			<div class='auth-col-bg'></div>
+		</section>
+	</main>
+
+	<!-- Footer -->
+	<?php require_once(__DIR__ . '/../components/footer.inc.php'); ?>
+</body>
+
+</html>
