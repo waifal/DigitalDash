b39eb5e343b792ce318dbcd83b614235a8d680c6
Waiwhakaira Falwasser <waifalcode@gmail.com>
2025-05-02 22:12:45 +1200

    Sanitize user input by allowing only letters in validateUser()

--- php/includes/functions.inc.php
+++ php/includes/functions.inc.php
@@ -1,9 +1,12 @@
 <?php
 
+
 function validateUser(?string $userInput): ?string {
 	if ($userInput === null) {
 		return null;
 	}
+
+	$userInput = preg_replace('/[^a-zA-Z]/', '', $userInput);
 
 	return stripslashes(trim(htmlspecialchars($userInput, ENT_QUOTES, 'UTF-8')));
 }
