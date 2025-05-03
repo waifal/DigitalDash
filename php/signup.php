5c85db38bdb46111d3e48c2f99930277b5f99827
Waiwhakaira Falwasser <waifalcode@gmail.com>
2025-05-03 12:59:39 +1200

    Declare and initialize signup variables

--- php/signup.php
+++ php/signup.php
@@ -32,3 +32,13 @@ switch ($_SESSION["logged_in"]) {
 }
 
 require_once(__DIR__ . '/includes/functions.inc.php');
+
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+	$fname = validate_user($_POST['fname'] ?? '');
+	$lname = validate_user($_POST['lname'] ?? '');
+	$email = validate_email($_POST['email'] ?? '');
+	$password = validate_password($_POST['password'] ?? '');
+	$pwd_confirm = $_POST['pwd_confirm'];
+	$terms_and_conditions = validate_checkbox('terms_and_conditions');
+	$privacy_policy = validate_checkbox('privacy_policy');
+}
