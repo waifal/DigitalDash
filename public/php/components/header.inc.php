951ddec411a360aac3b2046fe59d88a3c243af07
Waiwhakaira Falwasser <waifalcode@gmail.com>
2025-05-03 21:38:52 +1200

    Correct the href URL for the stylesheet

--- public/php/components/header.inc.php
+++ public/php/components/header.inc.php
@@ -8,7 +8,7 @@
 	<title>DigitalDash</title>
 
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css">
-	<link rel="stylesheet" href="../css/styles.css">
+	<link rel="stylesheet" href="../../css/styles.css">
 </head>
 
 <body>
