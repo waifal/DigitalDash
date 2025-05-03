ceee63e170c5105fe1b20e4e2c4dcf7018021456
Waiwhakaira Falwasser <waifalcode@gmail.com>
2025-05-03 19:14:09 +1200

    Make minor adjustments to SQL

--- db/dduserdb.sql
+++ db/dduserdb.sql
@@ -3,10 +3,10 @@ CREATE DATABASE dduserdb;
 USE dduserdb;
 
 CREATE TABLE tbluser (
-    userid INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT AUTO_INCREMENT PRIMARY KEY,
     firstname VARCHAR(30) NOT NULL,
     lastname VARCHAR(30) NOT NULL,
-    email VARCHAR(100) NOT NULL UNIQUE,
+    email VARCHAR(255) NOT NULL UNIQUE,
     password VARCHAR(255) NOT NULL, 
     agreed_terms TINYINT(1) NOT NULL DEFAULT 0,
     agreed_privacy TINYINT(1) NOT NULL DEFAULT 0
