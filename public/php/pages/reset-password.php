181f89418d6985f4e1f5b86298ff13c6325c1ca5
Waiwhakaira Falwasser <waifalcode@gmail.com>
2025-05-04 20:22:07 +1200

    Initialize password reset process

--- public/php/pages/reset-password.php
+++ public/php/pages/reset-password.php
@@ -6,10 +6,10 @@ if (!isset($_SESSION['csrf_token'])) {
 	$_SESSION['csrf_token'] = bin2hex(random_bytes(32));
 }
 
-// if ($_SESSION['logged_in'] === false) {
-// 	header('Location: login.php');
-// 	exit;
-// }
+if ($_SESSION['logged_in'] === false) {
+	header('Location: login.php');
+	exit;
+}
 
 $_SESSION['email_is_correct'] = $_SESSION['email_is_correct'] ?? false;
 
