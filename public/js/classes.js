f9b84da5c31db244e0d15a7edea4198c481d36e1
Waiwhakaira Falwasser <waifalcode@gmail.com>
2025-05-04 14:36:00 +1200

    Create Modal class with dynamic content and close functionality

--- public/js/classes.js
+++ public/js/classes.js
@@ -3,4 +3,32 @@ class Modal {
         this.button = document.getElementById(targetId);
         this.text = text;
     }
+
+    showModal() {
+        const modalParent = document.createElement('div');
+
+        modalParent.className = "modal__container";
+        modalParent.innerHTML = `
+            <dialog class="modal">
+                <div>
+                    <button class="close-modal"><i class="fa-solid fa-xmark"></i></button>
+                </div>
+                <div>
+                    <p>${this.text}</p>
+                </div>
+            </dialog>
+        `;
+
+        document.body.prepend(modalParent);
+
+        const dialog = modalParent.querySelector('dialog');
+        const closeModalBtn = dialog.querySelector('.close-modal');
+
+        dialog.showModal();
+
+        closeModalBtn.addEventListener('click', () => modalParent.remove());
+        window.addEventListener('keydown', (event) => event.key === "Escape" ? modalParent.remove() : null);
+    }
 }
+
+export { Modal };
