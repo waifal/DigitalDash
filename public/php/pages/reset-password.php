44ccb8ff34737be7c7eb86689707c97918c6b43f
Waiwhakaira Falwasser <waifalcode@gmail.com>
2025-05-04 17:56:35 +1200

    Make minor changes

--- public/php/pages/reset-password.php
+++ public/php/pages/reset-password.php
@@ -6,16 +6,10 @@ if (!isset($_SESSION['csrf_token'])) {
 	$_SESSION['csrf_token'] = bin2hex(random_bytes(32));
 }
 
-if (!isset($_SESSION['logged_in'])) {
-	$_SESSION['logged_in'] = false;
-}
-
-if ($_SESSION['logged_in'] === true) {
-	header('Location: ../index.php');
-	exit;
-}
-
-$_SESSION['sign_in_page'] = true;
+// if ($_SESSION['logged_in'] === false) {
+// 	header('Location: login.php');
+// 	exit;
+// }
 
 require_once(__DIR__ . '/../components/header.inc.php');
 require_once(__DIR__ . '/../components/nav.inc.php');
