66d0e35bb05238029493327313d67cc54adba0db
Waiwhakaira Falwasser <waifalcode@gmail.com>
2025-05-05 17:04:30 +1200

    Add some starter code to the index.php file

--- public/php/index.php
+++ public/php/index.php
@@ -1,1 +1,14 @@
-<h1>Index.php</h1>
+<?php
+
+session_start();
+
+
+require_once(__DIR__ . '/../components/header.inc.php');
+require_once(__DIR__ . '/../components/nav.inc.php');
+?>
+
+<main>
+	<h1>Index.php</h1>
+</main>
+
+<?php require_once(__DIR__ . '/../components/footer.inc.php'); ?>
