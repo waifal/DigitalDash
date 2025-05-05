0ab54ad3f42bca623543a0e389cfa4cedc016ee4
Waiwhakaira Falwasser <waifalcode@gmail.com>
2025-05-05 15:53:03 +1200

    Enhance password reset process with email verification

--- php/check-email.inc.php
+++ php/check-email.inc.php
@@ -1,26 +1,18 @@
 <?php
 
 require_once(__DIR__ . '/includes/functions.inc.php');
+require_once(__DIR__ . '/send_reset_email.php');
 
 if ($_SERVER["REQUEST_METHOD"] === "POST") {
 	$email = validate_email($_POST['email'] ?? '');
 
-	var_dump($email);
-
-	if (isset($_SESSION['user_id'])) {
-		$_SESSION['email_is_correct'] = check_user_email($_SESSION['user_id'], $email) ?? false;
-
-		var_dump($_SESSION['email_is_correct']);
-
-		if (!$_SESSION['email_is_correct']) {
-			header('Location: ../public/php/pages/reset-password.php?error=invalid_email');
-			exit;
-		}
-
-		header('Location: ../public/php/pages/reset-password.php');
+	if (send_reset_link($email)) {
+		ob_start();
+		sendResetEmail($email, $_SESSION['token']);
+		ob_end_clean();
+		header('Location: ../public/php/pages/reset-password.php?success=email_sent');
 		exit;
 	} else {
-		$_SESSION['email_is_correct'] = false;
 		header('Location: ../public/php/pages/reset-password.php?error=invalid_email');
 		exit;
 	}
