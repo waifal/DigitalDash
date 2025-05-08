fb13335665e89ad3b9e049a0d2ecbf514ac4dae1
Waiwhakaira Falwasser <waifalcode@gmail.com>
2025-05-09 01:16:45 +1200

    Conditionally load scripts.js based on user session

--- public/php/components/footer.inc.php
+++ public/php/components/footer.inc.php
@@ -1,4 +1,8 @@
-<script src="../../js/scripts.js" type="module"></script>
+<?php if (!isset($_SESSION['user_id']) || $_SESSION['logged_in'] !== true): ?>
+    <script src="../../js/scripts.js" type="module"></script>
+<?php else: ?>
+    <script src="../js/scripts.js" type="module"></script>
+<?php endif;?>
 </body>
 
 </html>
