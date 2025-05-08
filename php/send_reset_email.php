a681d27935097d50d64bde249fa511f1c4104cb6
Waiwhakaira Falwasser <waifalcode@gmail.com>
2025-05-09 00:43:17 +1200

    Enhanced email template in `send_reset_email.php`

--- php/send_reset_email.php
+++ php/send_reset_email.php
@@ -15,7 +15,7 @@ function sendResetEmail($email, $token) {
 	$reset_link = "http://localhost/projects/DigitalDash/public/php/pages/reset-password.php?token=$token"; // Change URL on deployment
 
 	try {
-		$mail->SMTPDebug = 2; // Set to 0 for production, 2 for debugging
+		$mail->SMTPDebug = 0; // Set to 0 for production, 2 for debugging
 
 		$mail->CharSet = "UTF-8";
 		$mail->isSMTP();
@@ -30,18 +30,24 @@ function sendResetEmail($email, $token) {
 		$mail->addAddress($email);
 
 		$mail->isHTML(true);
-		$mail->Subject = "Password Reset";
+		$mail->Subject = "Password Reset Request - DigitalDash Account";
 		$mail->Body = "
-		<p>Dear User,</p>
-		<p>We received a request to reset your password. If this was you, please click the link below to proceed:</p>
-		<p>
-			<a href='$reset_link' style='color:#0d6efd; font-weight:bold; text-decoration-line: underline;'>Reset Password</a>
-		</p>
-		<p>If you did not request this change, you can safely ignore this email. Your account will remain secure.</p>
-		<br>
-		<p>Best regards,</p>
-		<p><strong>DigitalDash Support Team</strong></p>";
-		$mail->AltBody = "Click this link to reset your password: $reset_link";
+		<div style='font-family: Arial, sans-serif; color: #333;'>
+			<p>Dear User,</p>
+			<p>We have received a request to reset the password for your DigitalDash account. To proceed with this request, please click the secure link below:</p>
+			<p style='margin: 25px 0;'>
+				<a href='$reset_link' style='background-color: #2dc489; color: white; padding: 12px 24px; text-decoration: none; border-radius: 4px; display: inline-block;'>Reset Your Password</a>
+			</p>
+			<p><strong>Important:</strong> If you did not initiate this request, please disregard this email. Your account security remains intact.</p>
+			<p style='margin-top: 20px;'>
+			Best regards,
+			<br>
+			<strong>DigitalDash Support Team</strong>
+			</p>
+			<hr style='border: none; border-top: 1px solid #eee; margin: 20px 0;'>
+			<p style='color: #666; font-size: 12px;'>This is an automated message. Please do not reply to this email.</p>
+			</div>";
+		$mail->AltBody = "A password reset was requested for your DigitalDash account. To reset your password, please visit: $reset_link\n\nIf you did not request this change, please ignore this email.\n\nBest regards,\nThe DigitalDash Team";
 
 		if ($mail->send()) {
 			error_log("Email sent successfully to $email");
