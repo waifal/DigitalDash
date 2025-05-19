eacf38763da1c049903e3b376b79c3747e2ba7ad
Waiwhakaira Falwasser <waifalcode@gmail.com>
2025-05-19 16:31:09 +1200

    Fix indentation and formatting in header component

--- public/php/components/header.inc.php
+++ public/php/components/header.inc.php
@@ -5,9 +5,11 @@
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="icon" href="../../assets/images/logo/logo_color_transparent_logo.png">
+	<link rel="icon" href="../../assets/images/logo/logo_color_transparent_logo.png">
 	<title>DigitalDash</title>
 
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.css"
+		integrity="sha384-tViUnnbYAV00FLIhhi3v/dWt3Jxw4gZQcNoSCxCIFNJVCx7/D55/wXsrNIRANwdD" crossorigin="anonymous">
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css">
 	<?php
 	if (isset($_SESSION['index'])):
