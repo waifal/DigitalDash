ef1a23091fc4b0fef409e19950c62a6017f94bef
Waiwhakaira Falwasser <waifalcode@gmail.com>
2025-05-28 18:40:49 +1200

    Add account-setting css

--- public/php/components/header.inc.php
+++ public/php/components/header.inc.php
@@ -14,15 +14,16 @@
 
 	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.css"
 		integrity="sha384-tViUnnbYAV00FLIhhi3v/dWt3Jxw4gZQcNoSCxCIFNJVCx7/D55/wXsrNIRANwdD" crossorigin="anonymous">
-	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css"> <?php if ($_SESSION['index'] === true): ?>
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css">
+	<?php if ($_SESSION['index'] === true): ?>
 		<link rel="stylesheet" href="../css/homepage.css">
 		<link rel="stylesheet" href="../css/styles.css">
 		<link rel="stylesheet" href="../css/rwd.css">
 	<?php else: ?>
 		<link rel="stylesheet" href="../../css/styles.css">
 		<link rel="stylesheet" href="../../css/rwd.css">
+		<link rel="stylesheet" href="../../css/account-settings.css">
 	<?php endif; ?>
-
 	<?php if (isset($_SESSION['digital_walks']) && $_SESSION['digital_walks'] === true || basename($_SERVER['PHP_SELF']) === 'digital-walks.php'): ?>
 		<link href="https://unpkg.com/video.js/dist/video-js.min.css" rel="stylesheet">
 		<link href="https://unpkg.com/@videojs/themes@1/dist/forest/index.css" rel="stylesheet">
